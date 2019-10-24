c662d23f2fb8232c21d05bae675940aa0b2d9ea7
Alex H <hampton.alex10@gmail.com>
2019-10-24 11:01:01 -0600

    changed index file

---
+++ assets/javascript/game.js
@@ -0,0 +1,52 @@
+var targetNumber = "";
+var wins = 0; 
+var losses = 0; 
+var counter = 0; 
+var images = ["./assets/images/pinkcrystal.png", "./assets/images/purplecrystal.png", "./assets/images/yellowcrystal.png", "./assets/images/bluecrystal.png"]; 
+
+
+function randomNumber () {
+    randomNumber = Math.floor(Math.random() * 102) + 19; 
+}
+
+function crystalReset () {
+    for (var i = 0; i <images.length; i++) {
+        var crystal = $("<img>"); 
+        crystal.addClass("crystal"); 
+        crystal.attr("src", images[i]); 
+        crystal.attr("value", (Math.floor(Math.random() * 12)+ 1)); 
+        crystal.attr("height", "100"); 
+        $(".crystal-images").append(crystal); 
+    }
+}
+
+function htmlReset () {
+    $(".target-number").html(targetNumber); 
+    $(".win-lose-counter").html("<p>Wins: " + wins + "</p>" + "<p>Losses:" + losses + "</p>"); 
+    $(".scpre-number").html(counter); 
+    $(".crystal-images").empty(); 
+}
+
+function totalReset () {
+    randomNumber (); 
+    counter = 0; 
+    htmlReset (); 
+    crystalReset (); 
+}
+
+randomNumber (); 
+htmlReset (); 
+crystalReset (); 
+
+function clickCrystal () {
+    counter += parseInt ($(this).attr("value")); 
+    $(".score-number").html(counter); 
+    if (counter == targetNumber) {
+        wins++; 
+        totalReset (); 
+    }
+    else if (counter > targetNumber) {
+        losses ++; 
+        totalReset (); 
+    }; 
+}; 
